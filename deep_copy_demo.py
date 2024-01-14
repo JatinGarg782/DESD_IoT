183cd04b6947e0120353a68348b1d1a408e7f66f
Jatin Garg <148681984+JatinGarg782@users.noreply.github.com>
2024-01-14 20:16:59 +0530

    Update deep_copy_demo.py

--- deep_copy_demo.py
+++ deep_copy_demo.py
@@ -1,7 +1,8 @@
 import copy
 list1 = [1,2,3,4]
 list2 = list1
-
+#In Shallow Copy We are copying the whole list into the another list and when we append any value into the list it will also reflect into the another list.
+#This is the problem in shallow copy that change in one list will affect the other list also, ie. Pass By Reference
 print(id(list2))
 print(id(list1))
 
